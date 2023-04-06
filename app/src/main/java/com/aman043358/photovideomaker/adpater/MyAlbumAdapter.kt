1e963f4c850335ed15cdbb80f726bc963990040d
aman <aman043358sagar@gmail.com>
2023-04-06 17:48:23 +0530

    my album complete

---
+++ app/src/main/java/com/aman043358/photovideomaker/adpater/MyAlbumAdapter.kt
@@ -0,0 +1,53 @@
+package com.aman043358.photovideomaker.adpater
+
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.aman043358.photovideomaker.R
+import com.aman043358.photovideomaker.databinding.MyAlbumItemBinding
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import java.io.File
+
+class MyAlbumAdapter(val pathArr: ArrayList<String>): RecyclerView.Adapter<MyAlbumAdapter.ViewHolder>() {
+
+
+
+    inner class ViewHolder(val binding: MyAlbumItemBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(pos: Int){
+            Glide.with(binding.root)
+                .load( File(pathArr[pos]) )
+                .apply(RequestOptions().centerCrop())
+                .placeholder(R.drawable.ic_placeholder)
+                .error(R.drawable.ic_error_image)
+                .into(binding.iv)
+
+            binding.tv.text = File(pathArr[pos]).name
+            binding.root.setOnClickListener {
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.setDataAndType(Uri.parse(pathArr[pos] ),"video/*")
+                startActivity(binding.root.context,Intent.createChooser(intent,"play video using"),null)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+
+
+        val binding = MyAlbumItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(binding = binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun getItemCount(): Int {
+        return pathArr.size
+    }
+}
--- app/src/main/java/com/aman043358/photovideomaker/fragments/ImageSelectionFragment.kt
+++ app/src/main/java/com/aman043358/photovideomaker/fragments/ImageSelectionFragment.kt
@@ -1,32 +1,63 @@
 package com.aman043358.photovideomaker.fragments
 
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.aman043358.photovideomaker.R
+import com.aman043358.photovideomaker.adpater.MyAlbumAdapter
 import com.aman043358.photovideomaker.databinding.FragmentImageSelectionBinding
+import java.io.File
 
-class ImageSelectionFragment : Fragment() {
+class ImageSelectionFragment : Fragment(R.layout.fragment_image_selection) {
 
-    private var _binding: FragmentImageSelectionBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    lateinit var binding: FragmentImageSelectionBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentImageSelectionBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-        return root
+        binding = FragmentImageSelectionBinding.bind(view)
+
+
+
+
+
+
+
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+
+    fun getVideoFilesPath(): ArrayList<String> {
+
+        val videoPathArr = arrayListOf<String>()
+        val internalStoragePath = File(Environment.getExternalStorageDirectory().absolutePath)
+        val folderPath = File(internalStoragePath, "Test")
+
+        if (folderPath.exists() && folderPath.isDirectory) {
+            val listFiles = folderPath.listFiles()
+
+            if (listFiles != null) {
+                for (file in listFiles) {
+                    if (file.name.endsWith(".mp4")) {
+                        videoPathArr.add(file.path)
+
+                    }
+                }
+                return videoPathArr
+            } else {
+                Toast.makeText(binding.root.context, "no files found", Toast.LENGTH_SHORT).show()
+                return arrayListOf()
+
+            }
+
+        } else {
+            Toast.makeText(binding.root.context, "folder not found", Toast.LENGTH_SHORT).show()
+            return arrayListOf()
+        }
     }
+
 }
