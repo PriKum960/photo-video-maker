2097345f48a8d28d13f56928fb7b45854570394c
Anurag Sagar <anurag983sagar@gmail.com>
2023-04-06 09:28:09 +0530

    basic structure

---
+++ app/src/main/java/com/aman043358/photovideomaker/fragments/VideoAlbumFragment.kt
@@ -0,0 +1,33 @@
+package com.aman043358.photovideomaker.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.aman043358.photovideomaker.databinding.FragmentVideoAlbumBinding
+
+class VideoAlbumFragment : Fragment() {
+
+    private var _binding: FragmentVideoAlbumBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentVideoAlbumBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
